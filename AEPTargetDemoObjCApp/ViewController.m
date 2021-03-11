abe0c102ce0f94a60cce09b9a3ac44996dee52a3
pravin <pravinpk0707@gmail.com>
2021-03-11 13:21:29 -0800

    ObjC App UI

--- AEPTargetDemoObjCApp/ViewController.m
+++ AEPTargetDemoObjCApp/ViewController.m
@@ -8,6 +8,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *lblThirdParty;
+@property (weak, nonatomic) IBOutlet UILabel *lblTntId;
+@property (weak, nonatomic) IBOutlet UITextField *textThirdPartyID;
 
 @end
 
@@ -18,5 +21,39 @@
     // Do any additional setup after loading the view.
 }
 
+
+- (IBAction)prefetchClicked:(id)sender {
+
+}
+
+- (IBAction)locationDisplayedClicked:(id)sender {
+    
+}
+
+- (IBAction)locationClicked:(id)sender {
+    
+}
+
+- (IBAction)resetExperienceClicked:(id)sender {
+    [AEPMobileTarget resetExperience];
+}
+
+- (IBAction)getThirdPartyClicked:(id)sender {
+    [AEPMobileTarget getThirdPartyIdWithCompletion:^(NSString *thirdPartyID, NSError *error){
+        [self.lblThirdParty setText:thirdPartyID];
+    }];
+}
+
+- (IBAction)getTntIDClicked:(id)sender {
+    [AEPMobileTarget getTntIdWithCompletion:^(NSString *tntID, NSError *error){
+        [self.lblTntId setText:tntID];
+    }];
+}
+
+- (IBAction)setThirdPartyClicked:(id)sender {
+    if(![_textThirdPartyID.text isEqualToString:@""]) {
+        [AEPMobileTarget setThirdPartyId:_textThirdPartyID.text];
+    }
+}
 
 @end
