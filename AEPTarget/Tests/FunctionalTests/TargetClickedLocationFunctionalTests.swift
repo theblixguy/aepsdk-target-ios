0fdeeabccd2419682547ae428e25208905b329fb
Yansong Yang <yanyang@adobe.com>
2021-04-08 13:47:48 -0500

    add tests for "LocationClicked" API (#90)

--- AEPTarget/Tests/FunctionalTests/TargetClickedLocationFunctionalTests.swift
+++ AEPTarget/Tests/FunctionalTests/TargetClickedLocationFunctionalTests.swift
@@ -13,6 +13,7 @@
 @testable import AEPCore
 @testable import AEPServices
 @testable import AEPTarget
+import SwiftyJSON
 import XCTest
 
 class TargetClickedLocationFunctionalTests: TargetFunctionalTestsBase {
@@ -146,6 +147,182 @@ class TargetClickedLocationFunctionalTests: TargetFunctionalTestsBase {
         // verifies the Target's shared state
         XCTAssertEqual(1, mockRuntime.createdSharedStates.count)
         XCTAssertEqual("DE03D4AD-1FFE-421F-B2F2-303BF26822C1.35_0", mockRuntime.createdSharedStates[0]?["tntid"] as? String)
+    }
+
+    func testLocationClicked_afterRetrievedTheSameLocationContent() {
+        // mocked network response
+        let responseString = """
+            {
+              "status": 200,
+              "id": {
+                "tntId": "DE03D4AD-1FFE-421F-B2F2-303BF26822C1.35_0",
+                "marketingCloudVisitorId": "61055260263379929267175387965071996926"
+              },
+              "requestId": "01d4a408-6978-48f7-95c6-03f04160b257",
+              "client": "acopprod3",
+              "edgeHost": "mboxedge35.tt.omtrdc.net",
+              "execute": {
+                "mboxes": [
+                  {
+                    "index": 0,
+                    "name": "t_test_01",
+                    "options": [
+                      {
+                        "content": {
+                          "key1": "value1"
+                        },
+                        "type": "json",
+                        "eventToken": "uR0kIAPO+tZtIPW92S0NnWqipfsIHvVzTQxHolz2IpSCnQ9Y9OaLL2gsdrWQTvE54PwSz67rmXWmSnkXpSSS2Q=="
+                      }
+                    ],
+                    "analytics" : {
+                        "payload" : {"pe" : "tnt", "tnta" : "33333:1:0|12121|1,38711:1:0|1|1"}
+                    }
+                  }
+                ]
+              }
+            }
+        """
+
+        let requestDataArray: [[String: Any]?] = [
+            TargetRequest(mboxName: "t_test_01", defaultContent: "default", targetParameters: TargetParameters(parameters: ["mbox-parameter-key1": "mbox-parameter-value1"])),
+        ].map {
+            $0.asDictionary()
+        }
+
+        let data: [String: Any] = [
+            "request": requestDataArray,
+            "targetparams": TargetParameters(profileParameters: mockProfileParam).asDictionary() as Any,
+        ]
+        let loadRequestEvent = Event(name: "", type: "", source: "", data: data)
+
+        // creates a configuration's shared state
+        mockRuntime.simulateSharedState(extensionName: "com.adobe.module.configuration", event: loadRequestEvent, data: (value: mockConfigSharedState, status: .set))
+
+        // creates a lifecycle's shared state
+        mockRuntime.simulateSharedState(extensionName: "com.adobe.module.lifecycle", event: loadRequestEvent, data: (value: mockLifecycleData, status: .set))
+
+        // creates an identity's shared state
+        mockRuntime.simulateSharedState(extensionName: "com.adobe.module.identity", event: loadRequestEvent, data: (value: mockIdentityData, status: .set))
+
+        // registers the event listeners for Target extension
+        target.onRegistered()
+
+        // override network service
+        let mockNetworkService = TestableNetworkService()
+        ServiceProvider.shared.networkService = mockNetworkService
+        mockNetworkService.mock { _ in
+            let validResponse = HTTPURLResponse(url: URL(string: "https://amsdk.tt.omtrdc.net/rest/v1/delivery")!, statusCode: 200, httpVersion: nil, headerFields: nil)
+            return (data: responseString.data(using: .utf8), response: validResponse, error: nil)
+        }
+        guard let eventListener: EventListener = mockRuntime.listeners["com.adobe.eventType.target-com.adobe.eventSource.requestContent"] else {
+            XCTFail()
+            return
+        }
+        XCTAssertTrue(target.readyForEvent(loadRequestEvent))
+        XCTAssertTrue(target.targetState.loadedMboxJsonDicts.isEmpty)
+        // handles retrieve location content event
+        eventListener(loadRequestEvent)
+        XCTAssertEqual(1, target.targetState.loadedMboxJsonDicts.count)
+        XCTAssertTrue(Set(target.targetState.loadedMboxJsonDicts.keys) == Set([
+            "t_test_01",
+        ]))
+        // Build the location data
+        let locationClickedEvent = Event(name: "", type: "", source: "", data: ["name": "t_test_01", TargetConstants.EventDataKeys.IS_LOCATION_CLICKED: true])
+
+        mockNetworkService.resolvers.removeAll()
+        mockNetworkService.mock { _ in
+            XCTFail()
+            return nil
+        }
+        // handles the location displayed event
+        eventListener(locationClickedEvent)
+    }
+
+    func testLocationClicked_withNoPrefetchedMbox() {
+        // mocked network response
+        let responseString = """
+            {
+              "status": 200,
+              "id": {
+                "tntId": "DE03D4AD-1FFE-421F-B2F2-303BF26822C1.35_0",
+                "marketingCloudVisitorId": "61055260263379929267175387965071996926"
+              },
+              "requestId": "01d4a408-6978-48f7-95c6-03f04160b257",
+              "client": "acopprod3",
+              "edgeHost": "mboxedge35.tt.omtrdc.net",
+              "prefetch": {
+                "mboxes": [
+                  {
+                    "index": 0,
+                    "name": "Drink_1",
+                    "options": [
+                      {
+                        "content": {
+                          "key1": "value1"
+                        },
+                        "type": "json",
+                        "eventToken": "uR0kIAPO+tZtIPW92S0NnWqipfsIHvVzTQxHolz2IpSCnQ9Y9OaLL2gsdrWQTvE54PwSz67rmXWmSnkXpSSS2Q=="
+                      }
+                    ]
+                  }
+                ]
+              }
+            }
+        """
+
+        // builds the prefetch event
+        let prefetchDataArray: [[String: Any]?] = [
+            TargetPrefetch(name: "Drink_1"),
+        ].map {
+            $0.asDictionary()
+        }
+
+        let data: [String: Any] = [
+            "prefetch": prefetchDataArray,
+        ]
+        let prefetchEvent = Event(name: "", type: "", source: "", data: data)
+
+        // creates a configuration's shared state
+        mockRuntime.simulateSharedState(extensionName: "com.adobe.module.configuration", event: prefetchEvent, data: (value: mockConfigSharedState, status: .set))
+
+        // creates a lifecycle's shared state
+        mockRuntime.simulateSharedState(extensionName: "com.adobe.module.lifecycle", event: prefetchEvent, data: (value: mockLifecycleData, status: .set))
+
+        // creates an identity's shared state
+        mockRuntime.simulateSharedState(extensionName: "com.adobe.module.identity", event: prefetchEvent, data: (value: mockIdentityData, status: .set))
+
+        // registers the event listeners for Target extension
+        target.onRegistered()
+
+        // override network service
+        let mockNetworkService = TestableNetworkService()
+        ServiceProvider.shared.networkService = mockNetworkService
+        mockNetworkService.mock { _ in
+            let validResponse = HTTPURLResponse(url: URL(string: "https://amsdk.tt.omtrdc.net/rest/v1/delivery")!, statusCode: 200, httpVersion: nil, headerFields: nil)
+            return (data: responseString.data(using: .utf8), response: validResponse, error: nil)
+        }
+        guard let eventListener: EventListener = mockRuntime.listeners["com.adobe.eventType.target-com.adobe.eventSource.requestContent"] else {
+            XCTFail()
+            return
+        }
+        XCTAssertTrue(target.readyForEvent(prefetchEvent))
+        // handles the prefetch event
+        eventListener(prefetchEvent)
+        XCTAssertEqual(1, target.targetState.prefetchedMboxJsonDicts.count)
+        XCTAssertTrue(Set(target.targetState.prefetchedMboxJsonDicts.keys) == Set([
+            "Drink_1",
+        ]))
+        // Build the location data
+        let locationClickedEvent = Event(name: "", type: "", source: "", data: ["name": "Drink_2", TargetConstants.EventDataKeys.IS_LOCATION_CLICKED: true])
+
+        mockNetworkService.resolvers.removeAll()
+        mockNetworkService.mock { _ in
+            XCTFail()
+            return nil
+        }
+        // handles the location displayed event
+        eventListener(locationClickedEvent)
     }
 
     func testLocationClicked_no_mbox() {
