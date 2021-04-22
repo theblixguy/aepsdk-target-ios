ef8b7402fe6fb43aa36cde1f936cc02d783d6850
Yansong Yang <yanyang@adobe.com>
2021-04-22 16:58:02 -0500

    update codecov step (#98)
    * update codecov step
    
    * fix remove comments

--- Script/codecov_downloader.sh
+++ Script/codecov_downloader.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 curl -s https://codecov.io/bash >codecov
-VERSION=$(grep 'VERSION=\".*\"' codecov | cut -d'"' -f2)
+VERSION=$(grep 'VERSION=\"[0-9\.]*\"' codecov | cut -d'"' -f2);
 for i in 1 256 512; do
-  shasum -a $i -c <(curl -s https://raw.githubusercontent.com/codecov/codecov-bash/${VERSION}/SHA${i}SUM)
+  shasum -a $i -c --ignore-missing <(curl -s "https://raw.githubusercontent.com/codecov/codecov-bash/${VERSION}/SHA${i}SUM") ||
+    shasum -a $i -c <(curl -s "https://raw.githubusercontent.com/codecov/codecov-bash/${VERSION}/SHA${i}SUM" | grep -w "codecov")
 done
